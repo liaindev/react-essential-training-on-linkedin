af8e66f3e9f99e16e7fdfa898618c5bcf3ef02db
MrsWebDev <liliia.shashina@gmail.com>
2020-02-12 00:13:42 +0100

    Create React elements

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+// import the React library in order to use it
 import React from 'react';
+// import ReactDOM to render our React component to the DOM
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 
+// styles for our title
+var myStyle = {
+  backgroundColor: 'black',
+  color: 'white',
+  fontFamily: 'Arial'
+}
+
+// create React element with 3 arguments
+const title = React.createElement(
+  'h1',
+  {id: 'title', className: 'header', style: myStyle},
+  'Hello World'
+)
+
+// Use ReactDOM.render() to render this title to the DOM
 ReactDOM.render(
-  <App />,
+  title,
   document.getElementById('root')
-);
+)
+
