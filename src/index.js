0128c0695c1f4938a6c20e05118fad6b8c3e1412
MrsWebDev <liliia.shashina@gmail.com>
2020-02-12 00:47:23 +0100

    Refactor elements using JavaScript and XML (JSX)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
+// import the React library in order to use it
 import React from 'react';
+// import ReactDOM to render our React component to the DOM
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 
+// styles for our title
+var myStyle = {
+  backgroundColor: 'black',
+  color: 'white',
+  fontFamily: 'Arial'
+}
+
+// Use ReactDOM.render() to render this title to the DOM
 ReactDOM.render(
-  <App />,
+  // use JSX tag-based syntax with curly braces {}
+  <div style={myStyle}>
+    <h1 id="heading-element">Hello, world!</h1>
+    <p>We're glad you're here</p>
+  </div>,
   document.getElementById('root')
-);
+)
+
