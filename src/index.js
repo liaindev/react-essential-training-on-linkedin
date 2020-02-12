90401221042433b3d4a617318006359f724e5c7b
MrsWebDev <liliia.shashina@gmail.com>
2020-02-12 03:21:49 +0100

    Display child components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-// create an arrow function with 3 properties
+// create an array of objects
+let bookList = [
+  {"title": "Hunger", "author": "Roxane Gay", "pages": 320},
+  {"title": "The Sun Also Rises", "author": "Ernest Hemingway", "pages": 260},
+  {"title": "White Teeth", "author": "Zadie Smith", "pages": 480},
+  {"title": "Cat's Cradle", "author": "Kurt Vonnegut", "pages": 304}
+]
+
+// create a new component as an arrow function with 3 properties
 const Book = ({title, author, pages}) => {
   return (
     <section>
@@ -12,20 +20,26 @@ const Book = ({title, author, pages}) => {
   )
 }
 
-// <Library /> is a parent component and it rendering each of the Book components
-const Library = () => {
+// <Library /> is a parent component and it rendering each of the Book components using bookList array
+// rendering an array of books with map() method
+const Library = ({books}) => {
   return (
     <div>
-      <Book title="The Sun Also Rises" author="Ernest Hemingway" pages={260}/>
-      <Book title="White Teeth" author="Zadie Smith" pages={480}/>
-      <Book title="Cat's Cradle" author="Kurt Vonnegut" pages={304}/>
+      {books.map (
+        (book, i) => <Book 
+                  // each of these data are being driven from props which come the bookList itself
+                  // added a unique key {i} for array elements
+                  key={i}
+                  title={book.title} 
+                  author={book.author} 
+                  pages={book.pages}/>
+      )}
     </div>
   )
 }
 
-// to render multiple different <Library /> components to the DOM
 render(
-  <Library />,
+  <Library books={bookList}/>,
   document.getElementById('root')
 )
 
