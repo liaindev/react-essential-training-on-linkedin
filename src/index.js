58998fe6827789a4d9176c6b90fc56eae0f44e01
MrsWebDev <liliia.shashina@gmail.com>
2020-02-12 19:29:01 +0100

    Pass state as props

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-// create an array of objects
 let bookList = [
   {"title": "Hunger", "author": "Roxane Gay", "pages": 320},
   {"title": "The Sun Also Rises", "author": "Ernest Hemingway", "pages": 260},
@@ -9,37 +8,32 @@ let bookList = [
   {"title": "Cat's Cradle", "author": "Kurt Vonnegut", "pages": 304}
 ]
 
-// create a new component as an arrow function with 3 properties
-const Book = ({title, author, pages}) => {
+const Book = ({title, author, pages, freeBookmark}) => {
   return (
     <section>
       <h2>{title}</h2>
       <p>by: {author}</p>
       <p>Pages: {pages} pages</p>
+      <p>Free Bookmark Today: {freeBookmark ? 'yes!' : 'no!'}</p>
     </section>
   )
 }
 
 class Library extends Component {
-  // the same as this.state = { open: false }
-  state = { open: false }
+  state = { 
+    open: true, 
+    freeBookmark: true 
+  }
 
-  /* we need to bind this for toggleOpenClosed before the constructor, 
-  but we can also do it using an arrow function,
-  because arrow function will automatically bind this inside the context of this function */
   toggleOpenClosed = () => {
-    // setState is asynchronous. Instead of object we are going to use a callback function
-    // prevState returns the object, so the object should be wrapped in another set of enclosing parentheses ()
     this.setState(prevState => ({
       open: !prevState.open
     }))
   }
 
   render () {
-    // state is just an object and it has as many keys as you set up 
     console.log(this.state)
 
-    // use ES6 destructuring
     const { books } = this.props
     return (
       <div>
@@ -50,7 +44,9 @@ class Library extends Component {
                     key={i}
                     title={book.title} 
                     author={book.author} 
-                    pages={book.pages}/>
+                    pages={book.pages}
+                    // pass down freeBoomark from state
+                    freeBookmark={this.state.freeBookmark}/>
         )}
       </div>
     )
