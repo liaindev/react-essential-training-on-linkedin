6c5c4cecdb0f43fba1a86a43c8995f35805661e1
MrsWebDev <liliia.shashina@gmail.com>
2020-02-12 02:48:08 +0100

    Compose multiple components

--- src/index.js
+++ src/index.js
@@ -1,52 +1,20 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-let skiData = {
-  total: 50,
-  powder: 20,
-  backcountry: 10,
-  goal: 100
-}
-
-// create a separate functions
-const getPercent = decimal => {
-  return decimal * 100 + '%'
-}
-const calcGoalProgress = (total, goal) => {
-  return getPercent(total/goal)
-}
-
-/* You can also create a component as a function.
-So using the JS syntax we can create a JS function 
-that returns JSX elements to be rendered */
-
-// create an arrow function to create component SkiDayCounter that returns some JSX elements, destructuring properties in {}
-const SkiDayCounter = ({total, powder, backcountry, goal}) => {
+const Library = () => {
   return (
-    <section>
-      <h1>Ski days</h1>
-      <div>
-        <p>Total days: {total}</p>
-      </div>
-      <div>
-        <p>Powder days: {powder}</p>
-      </div>
-      <div>
-        <p>Backcountry days: {backcountry}</p>
-      </div>
-      <div>
-        <p>Goal progress: {calcGoalProgress(total, goal)}</p>
-      </div>
-  </section>
+    <div>
+      Welcome to the Library!
+    </div>
   )
 }
 
+// to render multiple different <Library /> components to the DOM
 render(
-  <SkiDayCounter 
-    total={skiData.total}
-    powder={skiData.powder}
-    backcountry={skiData.backcountry}
-    goal={skiData.goal}/>,
+  <div>
+    <Library />
+    <Library />
+  </div>,
   document.getElementById('root')
 )
 
