ce18ad4e7f4e0a49209319bd79ce7f47c35a38c0
MrsWebDev <liliia.shashina@gmail.com>
2020-02-12 02:39:14 +0100

    Create function components - using arrow functions to minimize the code

--- src/index.js
+++ src/index.js
@@ -8,26 +8,34 @@ let skiData = {
   goal: 100
 }
 
+// create a separate functions
+const getPercent = decimal => {
+  return decimal * 100 + '%'
+}
+const calcGoalProgress = (total, goal) => {
+  return getPercent(total/goal)
+}
+
 /* You can also create a component as a function.
 So using the JS syntax we can create a JS function 
 that returns JSX elements to be rendered */
 
-// create an arrow function that returns some JSX elements
-const SkiDayCounter = (props) => {
+// create an arrow function to create component SkiDayCounter that returns some JSX elements, destructuring properties in {}
+const SkiDayCounter = ({total, powder, backcountry, goal}) => {
   return (
     <section>
       <h1>Ski days</h1>
       <div>
-        <p>Total days: {props.total}</p>
+        <p>Total days: {total}</p>
       </div>
       <div>
-        <p>Powder days: {props.powder}</p>
+        <p>Powder days: {powder}</p>
       </div>
       <div>
-        <p>Backcountry days: {props.backcountry}</p>
+        <p>Backcountry days: {backcountry}</p>
       </div>
       <div>
-        <p>Goal progress: {props.goal}</p>
+        <p>Goal progress: {calcGoalProgress(total, goal)}</p>
       </div>
   </section>
   )
