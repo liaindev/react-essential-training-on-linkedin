a6c2a5046cff350c8f3ca5979475d5a0d5e5cf7f
MrsWebDev <liliia.shashina@gmail.com>
2020-02-12 01:26:14 +0100

    Add component properties (Console)

--- src/index.js
+++ src/index.js
@@ -6,16 +6,21 @@ import ReactDOM from 'react-dom';
 class Message extends React.Component {
   // use render() method to render elements to the DOM
   render() {
+    // to see the props object with a message (msg) as a key
+    console.log(this.props)
     return (
       <div>
-        <h1>Hello Everyone</h1>
+        <h1>{this.props.msg}</h1>
       </div>
     )
   }
 }
 
 // Use ReactDOM.render() to render our Message component to the DOM
-ReactDOM.render(<Message />, document.getElementById('root')
+ReactDOM.render(
+  // age & msg are properties for the Message component
+  <Message age={50} msg="How are you?"/>, 
+  document.getElementById('root')
 )
 
 
