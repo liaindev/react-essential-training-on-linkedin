e7a9d8c477f3c0eb99fcacfc4965a6d052c4502a
MrsWebDev <liliia.shashina@gmail.com>
2020-02-27 19:44:36 +0100

    Fetching data

--- src/index.js
+++ src/index.js
@@ -33,44 +33,63 @@ const NotHiring = () =>
 class Library extends Component {
   state = { 
     open: true, 
-    freeBookmark: true,
-    hiring: false
+    freeBookmark: false,
+    hiring: true,
+    data: [],
+    loading: false
   }
 
   componentDidMount() {
-    console.log("The component is now mounted!")
+    this.setState({loading: true})
+    // fetch some data from a REST API
+    fetch('https://hplussport.com/api/products/order/price/sort/asc/qty/1')
+      .then(data => data.json())
+      .then(data => this.setState({data, loading: false}))
   }
 
-  componentDidUpdate() {
-    console.log("The component just updated!")
-  }
+	componentDidUpdate() {
+		console.log("The component just updated")
+	}
 
-  toggleOpenClosed = () => {
-    this.setState(prevState => ({
-      open: !prevState.open
-    }))
-  }
-
-  render () {
-    console.log(this.state)
-    const { books } = this.props
-    return (
-      <div>
-        {this.state.hiring ? <Hiring /> : <NotHiring />}
-        <h1>The library is {this.state.open ? 'open' : 'closed'}</h1>
-        <button onClick={this.toggleOpenClosed}>Change</button>
-        {books.map (
-          (book, i) => <Book 
-                    key={i}
-                    title={book.title} 
-                    author={book.author} 
-                    pages={book.pages}
-                    // pass down freeBoomark from state
-                    freeBookmark={this.state.freeBookmark}/>
-        )}
-      </div>
-    )
-  }
+	toggleOpenClosed = () => {
+		this.setState(prevState => ({
+			open: !prevState.open
+		}))
+	}
+	render() {
+		const { books } = this.props
+		return (
+			<div>
+				{this.state.hiring ? <Hiring /> : <NotHiring />}
+				{this.state.loading 
+					? "loading..."
+					: <div>
+						{this.state.data.map(product => {
+							return (
+								<div>
+									<h3>Library Product of the Week!</h3>
+									<h4>{product.name}</h4>
+									<img src={product.image} height={100}/>
+								</div>
+							)
+						})}
+						
+					</div>
+				}
+				<h1>The library is {this.state.open ? 'open' : 'closed'}</h1>
+				<button onClick={this.toggleOpenClosed}>Change</button>
+				{books.map(
+					(book, i) => 
+						<Book 
+							key={i}
+							title={book.title} 
+							author={book.author} 
+							pages={book.pages}
+							freeBookmark={this.state.freeBookmark}/>
+				)}
+			</div>
+		)
+	}
 }
 
 render(
