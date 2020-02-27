22c2d19e924d384996be798a803cd2fc4d7556eb
MrsWebDev <liliia.shashina@gmail.com>
2020-02-27 19:50:10 +0100

    Accessibility

--- src/index.js
+++ src/index.js
@@ -66,10 +66,10 @@ class Library extends Component {
 					: <div>
 						{this.state.data.map(product => {
 							return (
-								<div>
+								<div key={product.id}>
 									<h3>Library Product of the Week!</h3>
 									<h4>{product.name}</h4>
-									<img src={product.image} height={100}/>
+									<img alt={product.name} src={product.image} height={100}/>
 								</div>
 							)
 						})}
